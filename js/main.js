2e051b8e7835506c547743557e4c401b99d6b0e4
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-09-12 00:12:50 +0300

    correct hover effect errors on images work 7

--- js/main.js
+++ js/main.js
@@ -15,8 +15,92 @@ $(document).ready(function () {
         $("#imageofproducts, #detailsofproduct").toggle();
     })
 
+   //using jquery to add hover effect with text and border to each image in the poortfolio
+
+   //image work 4
+$('.project1').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p1').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p1').addClass('text');
+});
    
-// form...
+
+
+//image work 3
+$('.project2').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p2').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p2').addClass('text');
+});
+
+
+//image work 2
+$('.project3').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p3').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p3').addClass('text');
+});
+
+//image work 1
+
+$('.project4').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p4').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p4').addClass('text');
+});
+  /// images in the second row
+//image work 5
+
+$('.project5').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p5').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p5').addClass('text');
+});
+
+
+//image work 6
+$('.project6').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p6').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p6').addClass('text');
+});
+
+//image work 7
+
+$('.project7').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p7').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p7').addClass('text');
+});
+
+//image work 8
+
+$('.project8').hover(function () {
+    $(this).css("opacity", "0.6");
+    $('.p8').removeClass('text');
+}, function () {
+    $(this).css("opacity", "1");
+    $('.p8').addClass('text');
+});
+
+
+
+
+// form validation using javascript
 document.getElementById('submit').addEventListener('click', function () {
         var myName = $("#nameinputbox").val();
         var myMail = $("#emailinputbox").val();
@@ -31,7 +115,8 @@ document.getElementById('submit').addEventListener('click', function () {
 
 
 
-//add hover effect and text to each project
+//using jquery to add hover effect with text and border to each image in the poortfolio
+
 $('.project1').hover(function () {
     $(this).css("opacity", "0.6");
     $('.p1').removeClass('text');
@@ -82,7 +167,7 @@ $('.project7').hover(function () {
     $('.p7').removeClass('text');
 }, function () {
     $(this).css("opacity", "1");
-    $('.p2').addClass('text');
+    $('.p7').addClass('text');
 });
 
 $('.project8').hover(function () {
@@ -92,3 +177,5 @@ $('.project8').hover(function () {
     $(this).css("opacity", "1");
     $('.p8').addClass('text');
 });
+
+
